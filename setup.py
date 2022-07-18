3a50cefb04b069d68ae5f9549642b3363fcbf80b
Benjizhang <zzq0551@foxmail.com>
2022-07-18 19:27:11 +0800

    [ADD] setup.py to add root path

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ur_control',
+    version= '1.0',
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'pathlib',
+    ],
+    description="ur_control",
+    author="Benji Zhang",
+)
