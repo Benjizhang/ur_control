f07ccd0d4a8fd33f8e68d727c29c3264ec76b53d
Benjizhang <zzq0551@foxmail.com>
2022-07-19 22:40:41 +0800

    [MOD] add func to gen. spiral  traj.

--- functions/drawTraj.py
+++ functions/drawTraj.py
@@ -8,7 +8,7 @@ import numpy as np
 from tf import transformations as tfs
 
 
-def ur_spiralTraj(ur_control):
+def urSpiralTraj(ur_control):
     waypoints = []
     wpose = ur_control.group.get_current_pose().pose
     ## spiral trajectory
@@ -45,7 +45,7 @@ def genOtraj(cent,radius,start_angle,end_angle):
         y.append(cent[1]+radius*np.sin(theta))
     return x,y
 
-def ur_Otraj(ur_control):
+def urOtraj(ur_control):
     waypoints = []
     wpose = ur_control.group.get_current_pose().pose
 
@@ -64,6 +64,118 @@ def ur_Otraj(ur_control):
     ur_control.group.execute(plan, wait=True)
 
     return True
+
+## from current pos (circle center) to the given circle by the spiral trajectory
+def urCent2Circle(ur_control,radius,numLoop2circle):    
+    # coordinates
+    x = []
+    y = []
+
+    # the current position as the center of the circle
+    waypoints = []
+    wpose = ur_control.group.get_current_pose().pose
+    cent = [wpose.position.x, wpose.position.y]
+
+    # para. of spiral trajectory
+    a = 0    
+    delta_radius = radius/numLoop2circle # 1 loop/360 deg for moving delta_radius distance
+    b = delta_radius/(2*np.pi)
+
+    init_angle = np.pi/2
+    end_angle = 2*np.pi*numLoop2circle
+    # 36 segments for 180 deg
+    num_anlge = int(36*(end_angle/(np.pi)))
+
+    # generate spiral traj to the given circle
+    for theta in np.linspace(0,end_angle,num_anlge):
+        r = a+b*theta
+        wpose.position.x = cent[0]+r*np.cos(theta+init_angle)
+        wpose.position.y = cent[1]+r*np.sin(theta+init_angle)
+        waypoints.append(copy.deepcopy(wpose))
+
+        x.append(wpose.position.x)
+        y.append(wpose.position.y)
+    
+    (plan, fraction) = ur_control.group.compute_cartesian_path(waypoints,0.01,0.0)
+    ur_control.group.execute(plan, wait=True)
+    
+    return x,y,cent
+
+## from current pos (NOT the circle center) to the next circle with the given center
+## by spiral trajectory
+def urPt2Circle(ur_control,Ocent,radius,numLoop2circle):
+    # coordinates
+    x = []
+    y = []
+
+    # current pos    
+    waypoints = []
+    wpose = ur_control.group.get_current_pose().pose
+    curx = wpose.position.x
+    cury = wpose.position.y
+
+    # expand / shrink to the circle
+    dist2Cent = np.sqrt((Ocent[0]-curx)**2+(Ocent[1]-cury)**2)
+
+    delta_radius = (radius-dist2Cent)/numLoop2circle # 1 loop/360 deg for moving delta_radius distance
+    b = delta_radius/(2*np.pi)
+    
+    init_angle = np.arctan2(cury-Ocent[1],curx-Ocent[0])
+    end_angle = 2*np.pi*numLoop2circle
+    # 36 segments for 180 deg
+    num_anlge = int(36*(end_angle/(np.pi)))
+
+    # generate spiral traj to the given circle
+    for theta in np.linspace(0,end_angle,num_anlge):
+        r = dist2Cent+b*theta
+        wpose.position.x = Ocent[0]+r*np.cos(theta+init_angle)
+        wpose.position.y = Ocent[1]+r*np.sin(theta+init_angle)
+        waypoints.append(copy.deepcopy(wpose))
+
+        x.append(wpose.position.x)
+        y.append(wpose.position.y)
+    
+    (plan, fraction) = ur_control.group.compute_cartesian_path(waypoints,0.01,0.0)
+    ur_control.group.execute(plan, wait=True)
+    
+    return x,y
+
+## keep in given circle
+def keepCircle(ur_control,Ocent,numLoop):
+    # coordinates
+    x = []
+    y = []
+
+    # current pos
+    waypoints = []
+    wpose = ur_control.group.get_current_pose().pose
+    curx = wpose.position.x
+    cury = wpose.position.y
+
+
+    radius = np.sqrt((Ocent[0]-curx)**2+(Ocent[1]-cury)**2)
+    if round(radius,6) != 0:
+        init_angle = np.arctan2(cury-Ocent[1],curx-Ocent[0])
+        end_angle = 2*np.pi*numLoop
+        # 36 segments for 180 deg
+        num_anlge = int(36*(end_angle/(np.pi)))
+        # generate circle
+        for theta in np.linspace(0,end_angle,num_anlge):
+            wpose.position.x = Ocent[0]+radius*np.cos(theta+init_angle)
+            wpose.position.y = Ocent[1]+radius*np.sin(theta+init_angle)
+            waypoints.append(copy.deepcopy(wpose))
+
+            x.append(wpose.position.x)
+            y.append(wpose.position.y)
+        
+        (plan, fraction) = ur_control.group.compute_cartesian_path(waypoints,0.01,0.0)
+        ur_control.group.execute(plan, wait=True)
+
+        return x,y
+    else:
+        # do nothing
+        return curx,cury
+
 
 
 def move_along_boundary(ur_control,lim):
