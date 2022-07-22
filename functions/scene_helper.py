1796779f8183c0fade9dd6964323432e0dcfc62a
Benjizhang <zzq0551@foxmail.com>
2022-07-22 19:38:37 +0800

    [A] put class linstener into scene_helper.py

--- functions/scene_helper.py
+++ functions/scene_helper.py
@@ -136,3 +136,119 @@ def zero_ft_sensor():
         return resp1.success
     except rospy.ServiceException as e:
         print("Service call failed: %s"%e)
+
+import threading
+from robotiq_ft_sensor.msg import ft_sensor
+from control_msgs.msg import FollowJointTrajectoryActionResult as rlst
+class ft_listener():
+    def __init__(self):
+        
+        self.obj_pose_sub = rospy.Subscriber("robotiq_ft_sensor", ft_sensor, self.detect_callbak, queue_size=1)
+        self.result_status = rospy.Subscriber("/scaled_pos_joint_traj_controller/follow_joint_trajectory/result",rlst,self.callback, queue_size=1)
+        self.lock_read = threading.Lock()
+        self.sensor_data = None
+        self.force_val = None
+        self.force_dir = None
+        self.plan_finished =False
+
+    def detect_callbak(self,msg):
+        # msg = apriltag_ros.msg.AprilTagDetectionArray()
+        # for det in msg.detections:
+        #     det.pose
+        # a=msg.detections[0].pose.pose.pose
+        with self.lock_read:
+            # rospy.loginfo(f"I heard: {msg}")
+            # self.sensor_data = msg.xxx
+
+            # calculate the force values
+            self.force_val = math.sqrt((msg.Fx)**2+(msg.Fy)**2)
+
+            # calculate the force dir. (deg)
+            dir_rad = math.atan2(msg.Fy, msg.Fx)
+            self.force_dir = math.degrees(dir_rad)     
+    
+    def callback(self,msg):
+        # rospy.loginfo('finish callback')
+        with self.lock_read:
+            self.plan_finished =True
+    
+    def clear_finish_flag(self):
+        with self.lock_read:
+            self.plan_finished = False
+    
+    def read_finish_flag(self):
+        with self.lock_read:
+            return self.plan_finished
+    
+
+    # # rospy.loginfo(msg)
+    
+    def read_sensor(self):
+        with self.lock_read:
+            return self.sensor_data
+    
+    def get_force_val(self):
+        with self.lock_read:
+            return self.force_val
+
+    def get_force_dir(self):
+        with self.lock_read:
+            return self.force_dir
+
+## --- [force monitor] ---
+# def force_monitor():
+#     rospy.loginfo('clear_finish_flag')
+#     while not listener.read_finish_flag():                    
+#         ## measure the force val/dir
+#         f_val = listener.get_force_val()
+#         f_dir = listener.get_force_dir()
+#         if f_val is not None:
+#             ## most conservative way (most safe)
+#             if np.round(f_val,6) > CUR_SAFE_FORCE:
+#                 rospy.loginfo('==== Large Force Warning ==== \n')
+#                 ur_control.group.stop()
+#                 flargeFlag = True
+#                 break
+
+#             ## path distance
+#             cur_pos = ur_control.group.get_current_pose().pose
+#             curx = cur_pos.position.x
+#             cury = cur_pos.position.y
+#             dist = round(np.abs(cury - y_s_wldf),6)
+            
+#             # df_ls.append(round(f_val,6))
+#             # dr_ls.append(round(f_dir,6))
+#             # ds_ls.append(dist)
+#             # rospy.loginfo('ForceVal (N): {}'.format(f_val))
+#             # rospy.loginfo('Distance (m): {}'.format(dist))
+            
+#             ### only record the stable duration
+#             if round(dist - ds_min, 6) >= 0:
+#                 df_ls.append(round(f_val,6))
+#                 dr_ls.append(round(f_dir,6))
+#                 ds_ls.append(dist)
+            
+#             ## using jamming detector 1
+#             if len(df_ls) >= ite_bar and len(df_ls)%delta_ite==0:
+#                 ## Kalman Filter
+#                 fdhat, Pminus = smooth_fd_kf(df_ls)
+#                 ## Mean
+#                 fdmean = get_mean(df_ls)
+#                 ## absolute difference (N)
+#                 cur_abs_diff = round(abs(fdhat[-1] - fdmean[-1]),3)
+#                 ## detect jamming
+#                 # Mx, isJamming = jd1(fdhat, fdmean, 0.5, delta_ite) 
+#                 JDlib = JDLib(df_ls,ds_ls,fdhat,fdmean,diff_bar)
+#                 isJamming = JDlib.JD(JDid)
+#                 if isJamming:
+#                     rospy.loginfo('**== Jamming Detected ==** \n')
+#                     ## ----- v1.0 -----
+#                     ur_control.group.stop()
+#                     flargeFlag = True
+#                     ## plot jamming result
+#                     if isPlotJD:
+#                         ds_adv = round(ds_obj-ds_ls[-1], 3) # >0 in theory
+#                         title_str = 'Exp{}: ds [{},{}], Dep {}, Vel {}, Ite {}, Diff {}, Adv {}'.format(j,ds_min,np.inf,PENE_DEPTH,normalVelScale,len(df_ls),cur_abs_diff,ds_adv)
+#                         JDlib.plotJDRes(ds_obj,title_str,fig_path,j)
+#                     break
+    
